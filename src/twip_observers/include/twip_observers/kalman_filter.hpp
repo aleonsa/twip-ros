6ba72d442523302de1fdbd4908aa08517e1c18a6
aleonsa <aleons.jo@gmail.com>
2025-02-08 16:07:52 -0600

    kalman done

--- src/twip_observers/include/twip_observers/kalman_filter.hpp
+++ src/twip_observers/include/twip_observers/kalman_filter.hpp
@@ -4,33 +4,38 @@
 #include <rclcpp/rclcpp.hpp>
 #include <nav_msgs/msg/odometry.hpp>
 #include <sensor_msgs/msg/imu.hpp>
+#include <Eigen/Dense>
 
 class KalmanFilter : public rclcpp::Node
 {
 public:
     KalmanFilter(const std::string &name);
-
     void statePrediction();
-
     void measurementUpdate();
 
 private:
     void odomCallback(const nav_msgs::msg::Odometry &);
-
     void imuCallback(const sensor_msgs::msg::Imu &);
 
     rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
     rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imu_sub_;
     rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_pub_;
-    double mean_;
-    double variance_;
-    double motion_variance_;
-    double measurement_variance_;
-    double motion_;
+
+    // State variables and covariance matrices
+    Eigen::Vector3d mean_;                 // [roll, pitch, yaw]
+    Eigen::Matrix3d variance_;             // Covariance matrix of the state
+    Eigen::Matrix3d motion_variance_;      // Covariance matrix of the motion
+    Eigen::Matrix3d measurement_variance_; // Covariance matrix of the meas
+    Eigen::Vector3d motion_;               // [delta_roll, delta_pitch, delta_yaw]
+
     bool is_first_odom_;
-    double last_angular_z_;
-    double imu_angular_z_;
+    Eigen::Vector3d last_angles_; // Last measured angles
+    Eigen::Vector3d imu_angles_;  // Measured angles from the IMU
+
     nav_msgs::msg::Odometry kalman_odom_;
+
+    void quaternionToEuler(const geometry_msgs::msg::Quaternion &q, double &roll, double &pitch, double &yaw);
+    void eulerToQuaternion(const double roll, const double pitch, const double yaw, geometry_msgs::msg::Quaternion &q);
 };
 
 #endif // KALMAN_FILTER_HPP
--- src/twip_observers/src/kalman_filter.cpp
+++ src/twip_observers/src/kalman_filter.cpp
@@ -1,64 +1,98 @@
 #include "twip_observers/kalman_filter.hpp"
+#include <tf2/LinearMath/Matrix3x3.h>
+#include <tf2/LinearMath/Quaternion.h>
 
 using std::placeholders::_1;
 
 KalmanFilter::KalmanFilter(const std::string &name)
     : Node(name),
-      mean_(0.0),
-      variance_(1000.0),
-      motion_variance_(4.0),
-      measurement_variance_(0.5),
-      motion_(0.0),
-      is_first_odom_(true),
-      last_angular_z_(0.0),
-      imu_angular_z_(0.0)
+      is_first_odom_(true)
 {
-    odom_sub_ = create_subscription<nav_msgs::msg::Odometry>("twip_controller/odom_noisy", 10, std::bind(&KalmanFilter::odomCallback, this, _1));
-    imu_sub_ = create_subscription<sensor_msgs::msg::Imu>("imu/out", 1000, std::bind(&KalmanFilter::imuCallback, this, _1));
-    odom_pub_ = create_publisher<nav_msgs::msg::Odometry>("twip_controller/odom_kalman", 10);
+    // Initialize the state variables
+    mean_ = Eigen::Vector3d::Zero();
+    variance_ = Eigen::Matrix3d::Identity() * 1000.0;
+    motion_variance_ = Eigen::Matrix3d::Identity() * 4.0;
+    measurement_variance_ = Eigen::Matrix3d::Identity() * 0.5;
+    motion_ = Eigen::Vector3d::Zero();
+    last_angles_ = Eigen::Vector3d::Zero();
+    imu_angles_ = Eigen::Vector3d::Zero();
+
+    // Subscribers and publishers
+    odom_sub_ = create_subscription<nav_msgs::msg::Odometry>(
+        "twip_controller/odom_noisy", 10,
+        std::bind(&KalmanFilter::odomCallback, this, _1));
+
+    imu_sub_ = create_subscription<sensor_msgs::msg::Imu>(
+        "imu/out", 1000,
+        std::bind(&KalmanFilter::imuCallback, this, _1));
+
+    odom_pub_ = create_publisher<nav_msgs::msg::Odometry>(
+        "twip_controller/odom_kalman", 10);
 }
 
 void KalmanFilter::odomCallback(const nav_msgs::msg::Odometry &odom)
 {
     kalman_odom_ = odom;
+    double roll, pitch, yaw;
+    quaternionToEuler(odom.pose.pose.orientation, roll, pitch, yaw);
 
     if (is_first_odom_)
     {
-        last_angular_z_ = odom.twist.twist.angular.z;
-        mean_ = odom.twist.twist.angular.z;
+        last_angles_ << roll, pitch, yaw;
+        mean_ << roll, pitch, yaw;
         is_first_odom_ = false;
         return;
     }
 
-    motion_ = odom.twist.twist.angular.z - last_angular_z_;
+    Eigen::Vector3d current_angles(roll, pitch, yaw);
+    motion_ = current_angles - last_angles_;
 
     statePrediction();
     measurementUpdate();
 
-    // Update for the next iteration
-    last_angular_z_ = odom.twist.twist.angular.z;
+    // Actualizar para la siguiente iteración
+    last_angles_ = current_angles;
 
-    // Update and publish the filtered odom message
-    kalman_odom_.twist.twist.angular.z = mean_;
+    // Actualizar y publicar el mensaje de odometría filtrado
+    geometry_msgs::msg::Quaternion q;
+    eulerToQuaternion(mean_(0), mean_(1), mean_(2), q);
+    kalman_odom_.pose.pose.orientation = q;
     odom_pub_->publish(kalman_odom_);
 }
 
 void KalmanFilter::imuCallback(const sensor_msgs::msg::Imu &imu)
 {
-    imu_angular_z_ = imu.angular_velocity.z;
+    quaternionToEuler(imu.orientation, imu_angles_(0), imu_angles_(1), imu_angles_(2));
 }
 
 void KalmanFilter::measurementUpdate()
 {
-    mean_ = (measurement_variance_ * mean_ + variance_ * imu_angular_z_) / (variance_ + measurement_variance_);
-
-    variance_ = (variance_ * measurement_variance_) / (variance_ + measurement_variance_);
+    Eigen::Matrix3d K = variance_ * (variance_ + measurement_variance_).inverse();
+    mean_ = mean_ + K * (imu_angles_ - mean_);
+    variance_ = (Eigen::Matrix3d::Identity() - K) * variance_;
 }
 
 void KalmanFilter::statePrediction()
 {
     mean_ = mean_ + motion_;
     variance_ = variance_ + motion_variance_;
+}
+
+void KalmanFilter::quaternionToEuler(const geometry_msgs::msg::Quaternion &q, double &roll, double &pitch, double &yaw)
+{
+    tf2::Quaternion quat(q.x, q.y, q.z, q.w);
+    tf2::Matrix3x3 m(quat);
+    m.getRPY(roll, pitch, yaw);
+}
+
+void KalmanFilter::eulerToQuaternion(const double roll, const double pitch, const double yaw, geometry_msgs::msg::Quaternion &q)
+{
+    tf2::Quaternion quat;
+    quat.setRPY(roll, pitch, yaw);
+    q.x = quat.x();
+    q.y = quat.y();
+    q.z = quat.z();
+    q.w = quat.w();
 }
 
 int main(int argc, char *argv[])
